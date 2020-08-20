0e5143badc6c2aac3146cefad587fb86490def32
Fabiano Médice <65721704+fabianomedice@users.noreply.github.com>
2020-08-20 11:48:27 -0300

    Add files via upload

---
+++ Iniciação científica/Programação do Termo Higrômetro/teste leitura de arquivos.c
@@ -0,0 +1,15 @@
+/* CIN source file */
+
+#include "extcode.h"
+
+MgErr CINRun(void *arg1);
+
+MgErr CINRun(void *arg1)
+	{
+    int x, y;
+    
+    x= 1+y;
+	/* Insert code here */
+
+	return noErr;
+	}
