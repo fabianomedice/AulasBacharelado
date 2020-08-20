9f0f2cb977353efb20484c3d9f52a426044a32a8
Fabiano Médice <65721704+fabianomedice@users.noreply.github.com>
2020-08-20 16:35:53 -0300

    Add files via upload

---
+++ Curso de Sistemas Microcontrolados/Trabalhos/Pratica 13.X/build/default/production/modelo_main_define.o.d
@@ -0,0 +1,9 @@
+build/default/production/modelo_main_define.o : \
+	C:/Documents\ and\ Settings/Proprietário/Desktop/Curso\ PIC/Pratica\ 13.X/modelo_main_define.c \
+	C:/h/delays.h \
+	C:/Documents\ and\ Settings/Proprietário/Desktop/Curso\ PIC/Pratica\ 13.X/displayLCD.h \
+	C:/h/adc.h \
+	C:/h/p18cxxx.h \
+	C:/h/p18f4550.h \
+	C:/h/pconfig.h \
+
---
+++ Curso de Sistemas Microcontrolados/Trabalhos/Pratica 16.X/build/default/production/main_CronoIntRB4.o.d
@@ -0,0 +1,9 @@
+build/default/production/main_CronoIntRB4.o : \
+	C:/Documents\ and\ Settings/Proprietário/Desktop/Curso\ PIC/Pratica\ 16.X/main_CronoIntRB4.c \
+	C:/h/delays.h \
+	C:/Documents\ and\ Settings/Proprietário/Desktop/Curso\ PIC/Pratica\ 16.X/display7seg.h \
+	C:/h/timers.h \
+	C:/h/p18cxxx.h \
+	C:/h/p18f4550.h \
+	C:/h/pconfig.h \
+
